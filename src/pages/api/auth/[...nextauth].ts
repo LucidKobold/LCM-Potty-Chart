b256927b2ecc80ecdc24912088c7e10958cba551
Lucid Kobold <72232219+LucidKobold@users.noreply.github.com>
2022-07-29 10:00:48 -0400

    Added a profile page.

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -16,5 +16,5 @@ export default NextAuth({
     })
     // ...add more providers here
   ],
-  debug: true
+  debug: false
 });
---
+++ src/pages/profile/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useSession } from "next-auth/react"
+import { Box } from "@chakra-ui/react"
+import LoadingOverlay from "../../components/loading/LoadingOverlay";
+
+const UserProfile = (): JSX.Element => {
+  const { data: session, status } = useSession();
+
+  return status === "loading" ?
+    (
+      <LoadingOverlay />
+    )
+    :
+    session ?
+      (
+        <Box pt="50px">
+          {`Welcome to your profile ${session.user.email}`}
+        </Box>
+      )
+      :
+      (
+        <Box pt="50px">
+          {"You must be logged in to view your profile."}
+        </Box>
+      )
+}
+
+export default UserProfile;
