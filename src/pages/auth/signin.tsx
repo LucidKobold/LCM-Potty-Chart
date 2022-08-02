0b4149f72213924151c85be34fa93b09e7d63fb5
Lucid Kobold <72232219+LucidKobold@users.noreply.github.com>
2022-08-02 10:47:40 -0400

    Added logo to login. Added the check email message to verify page.

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Divider, VStack } from "@chakra-ui/react";
+import { Box, Divider, Heading, Image, VStack } from "@chakra-ui/react";
 import {
   getProviders,
   signIn,
@@ -23,15 +23,15 @@ const SignInPage = ({ providers }: SignInPageProps): JSX.Element => {
     <VStack
       h="100%"
       w="100%"
-      my="30vh"
+      my="20vh"
       justifyContent="center"
       alignContent="center"
     >
       <VStack
         h="auto"
-        w={{ base: "95vw", sm: "85vw", md: "80vw" }}
+        w={{ base: "95vw", sm: "90vw", md: "85vw", lg: "65vw" }}
         px={12}
-        py={8}
+        py={10}
         justifyContent="center"
         alignItems="center"
         border="1px solid #0068ff"
@@ -39,6 +39,49 @@ const SignInPage = ({ providers }: SignInPageProps): JSX.Element => {
         boxShadow="rgba(0, 134, 255, 0.5) 0px 0px 15px, rgba(0, 134, 255, 0.3) 0px 0px 3px 1px"
         spacing={4}
       >
+        <VStack
+          h="100%"
+          w="100%"
+          mb="3vh"
+          justifyContent="center"
+          alignContent="center"
+          spacing={6}
+        >
+          <Image
+            h="10rem"
+            w="10rem"
+            src="/images/logo.svg"
+            alt="LCM Potty Chart Logo"
+          />
+          <VStack
+            h="100%"
+            w="100%"
+            justifyContent="center"
+            alignContent="center"
+            textAlign="center"
+            spacing={0}
+          >
+            <Heading
+              as="h1"
+              fontSize={{ base: "3xl", md: "5xl" }}
+            >
+              {"Lucid Creations Media"}
+            </Heading>
+            <Heading
+              as="h2"
+              fontSize={{ base: "2xl", md: "4xl" }}
+            >
+              {"Code Name:"}
+            </Heading>
+            <Heading
+              as="h3"
+              fontSize={{ base: "2xl", md: "4xl" }}
+            >
+              {"Potty Chart"}
+            </Heading>
+          </VStack>
+          <Divider />
+        </VStack>
         {Object.values(providers).map((provider) => {
           const { id, name } = provider;
           return name !== "Email" ? (
