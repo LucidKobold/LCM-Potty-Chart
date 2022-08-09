fc895ee9ac3e1ab39c37ce77399b0a6b0b7c1e3c
Lucid Kobold <72232219+LucidKobold@users.noreply.github.com>
2022-08-09 11:10:55 -0400

    added apollo client

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { ApolloProvider } from "@apollo/client";
+import apolloClient from "../../lib/apollo";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
@@ -16,18 +18,20 @@ function LCMPottyChart({
     <React.StrictMode>
       <ChakraProvider theme={AppTheme}>
         <SessionProvider session={session}>
-          <Layout {...pageProps}>
-            <Head>
-              <title>{"LCM Potty Chart"}</title>
-              <meta
-                name="viewport"
-                content="width=device-width, user-scalable=yes, initial-scale=1.0"
-              />
-            </Head>
-            <Provider store={store}>
-              <Component {...pageProps} />
-            </Provider>
-          </Layout>
+          <ApolloProvider client={apolloClient}>
+            <Layout {...pageProps}>
+              <Head>
+                <title>{"LCM Potty Chart"}</title>
+                <meta
+                  name="viewport"
+                  content="width=device-width, user-scalable=yes, initial-scale=1.0"
+                />
+              </Head>
+              <Provider store={store}>
+                <Component {...pageProps} />
+              </Provider>
+            </Layout>
+          </ApolloProvider>
         </SessionProvider>
       </ChakraProvider>
     </React.StrictMode>
