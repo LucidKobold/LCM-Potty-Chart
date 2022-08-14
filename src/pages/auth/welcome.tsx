aa2a9063be6632154f7e51dfd9ed107e51fabf51
Lucid Kobold <72232219+LucidKobold@users.noreply.github.com>
2022-08-14 16:23:34 -0400

    Provide error messages when validating the user's activation status and token.

--- src/pages/auth/welcome.tsx
+++ src/pages/auth/welcome.tsx
@@ -1,10 +1,9 @@
 import React, { useEffect } from "react";
 import { useSession } from "next-auth/react";
-import { Box, Button } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import Activation from "../../components/welcome/Activation";
-import { gql, useLazyQuery, useQuery } from "@apollo/client";
+import { gql, useLazyQuery } from "@apollo/client";
 import { isBefore } from "date-fns";
-import { parseISO } from "date-fns/esm";
 
 // TODO: On this page users will see the tutorial, have a chance to edit their info, customize their privacy settings, and add their friends.
 
@@ -37,49 +36,77 @@ const NewUserPage = (): JSX.Element => {
     if (session) {
       return session.user.id;
     } else {
-      return ""
+      return "";
     }
-  }
+  };
 
-  const [loadTokenInfo, { called, loading, data }] = useLazyQuery(FETCH_VERIFYTOKEN, { variables: { userId: readUserId() } });
+  const [loadTokenInfo, { loading, data }] = useLazyQuery(FETCH_VERIFYTOKEN, {
+    variables: { userId: readUserId() }
+  });
 
   useEffect(() => {
     if (session) {
       loadTokenInfo();
     }
-  }, [loadTokenInfo, session])
-
+  }, [loadTokenInfo, session]);
 
   const validateToken = ({
     validated,
     userId,
     validatedAt,
     expires
-  }: Token): boolean => {
-    let flag = false;
+  }: Token): string => {
+    let message =
+      "An error ocurred when checking if your account was activated. Please try again.";
+    console.info(validated, userId, validatedAt, expires);
 
-    if (validatedAt === null) {
-      return flag;
+    if (
+      validated === undefined ||
+      validatedAt === undefined ||
+      expires === undefined ||
+      userId === undefined
+    ) {
+      message =
+        "Your account doesn't have an activation token. Please re-generate one. Contact support if this issue persists.";
+    }
+
+    if (!validated || validatedAt === null) {
+      message =
+        "Account not activated. Please check your email for the activation link.";
+    }
+
+    if (userId !== session.user.id) {
+      message =
+        "An error occurred when fetching your activation status. Please try again. Contact support if this issue persists.";
+    }
+
+    if (!isBefore(new Date(validatedAt), new Date(expires))) {
+      message =
+        "Your activation status is not valid. Please activate your account again by generating a new activation token.";
     }
 
     if (
-      userId === session.user.id &&
       validated &&
+      userId === session.user.id &&
       isBefore(new Date(validatedAt), new Date(expires))
     ) {
-      flag = true;
+      message = "Your account is activated.";
     }
 
-    return flag;
+    return message;
   };
 
   return session ? (
     <Box pt="50px">
       <Activation userId={session.user.id} />
-      {data && validateToken(data.getVerificationWithUserId) ? (
-        <Box>{"Account valid"}</Box>
+      {data ? (
+        <Box>{validateToken(data.getVerificationWithUserId)}</Box>
       ) : (
-        <Box>{"Account not valid"}</Box>
+        <Box>
+          {
+            "Your account doesn't have an activation token. Please re-generate one. Contact support if this issue persists."
+          }
+        </Box>
       )}
     </Box>
   ) : (
